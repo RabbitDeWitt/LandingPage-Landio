77f0a175f0f6c35d942426b57bb4aae4d8d26117
David Coelho <davidsocoelho@outlook.com>
2022-09-01 04:52:37 -0300

    Projeto Finazalido

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+/* NAVBAR */
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]")
+const header = document.querySelector("[data-header]")
+
+navToggleBtn.addEventListener('click', ()=>{
+  header.classList.toggle("active")
+})
